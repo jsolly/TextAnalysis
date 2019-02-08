3525101ceea385d7a864932fafdf7d594f203635
John Solly <jsolly@protonmail.com>
2019-02-08 13:24:21 -0800

    ip

--- word_cloud.py
+++ word_cloud.py
@@ -11,15 +11,21 @@ text = open('text_file.txt', "r", encoding="utf8").read()
 image_coloring = np.array(Image.open("lips.png"))
 stopwords = set(STOPWORDS)
 
-wc = WordCloud(background_color="white", mask=image_coloring,
-               stopwords=stopwords, width=800, height=400, max_words=90, font_path="Zombie_Holocaust.ttf")
+wc = WordCloud(background_color="black", mask=image_coloring,
+               stopwords=stopwords, width=800, height=400, max_words=2000, font_path="Zombie_Holocaust.ttf", random_state=9, contour_width=3, contour_color='red')
 
 wc.generate(text)
+#wc.to_file("black_lips.png")
 image_colors = ImageColorGenerator(image_coloring)
+
+plt.imshow(wc, interpolation='bilinear')
+plt.axis("off")
+plt.figure()
+plt.imshow(image_coloring, cmap=plt.cm.gray, interpolation='bilinear')
+plt.axis("off")
+plt.show()
 
 
 plt.imshow(wc.recolor(color_func=image_colors), interpolation="bilinear")
 plt.axis('off')
-plt.savefig("black_lips.png", transparent = True, bbox_inches = 'tight', pad_inches = 0)
-
-#wc.to_file("black_lips.png")
+plt.savefig("red_lips.png", transparent = True, bbox_inches = 'tight', pad_inches = 0)
